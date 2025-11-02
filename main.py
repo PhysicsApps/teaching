04fc39c4ff3c15c76f642bdd3618af553956e8a0
ptuemmler <paul.tuemmler@googlemail.com>
2025-11-02 20:01:47 +0100

    - fixed windows specific local building

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from pathlib import Path
-
+import platform
 
 def get_app_link(current_path, subdir: str):
     parts = current_path.parts
     html_filename = str(parts[-1]).split('.')[0]
     if subdir is not None:
         html_filename += '_' + subdir
-    return str(Path('apps', html_filename, 'index.html'))
+
+    if platform.system() == "Windows":
+        app_link =  str(Path('apps_' + html_filename, 'index.html'))
+    elif platform.system() == "Linux":
+        app_link =  str(Path('apps', html_filename, 'index.html'))
+    else:
+        raise ValueError("Unsupported operating system. This script only supports Windows and Linux.")
+    return app_link
 
 def define_env(env):
     """
