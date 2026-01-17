270c1a660dc0517f5e37b4f99bc97e5e5f129087
ptuemmler <paul.tuemmler@uni-rostock.de>
2026-01-17 17:35:35 +0100

    Added embed_code macro.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 import platform
 
-def get_app_link(current_path, subdir: str):
+def get_app_link(current_path, subdir: str, target: str):
     parts = current_path.parts
     html_filename = str(parts[-1]).split('.')[0]
     if subdir is not None:
         html_filename += '_' + subdir
 
     if platform.system() == "Windows":
-        app_link =  str(Path('apps_' + html_filename, 'index.html'))
+        app_link =  str(Path('apps_' + html_filename, target))
     elif platform.system() == "Linux":
-        app_link =  str(Path('apps', html_filename, 'index.html'))
+        app_link =  str(Path('apps', html_filename, target))
     else:
         raise ValueError("Unsupported operating system. This script only supports Windows and Linux.")
     return app_link
@@ -23,7 +23,18 @@ def define_env(env):
     @env.macro
     def embed_app(width='100%', height='600px', subdir: str = None):
         current_path = Path(str(getattr(env, 'page'))) # " /blog/plotly-penguins-app.html"
-        return f"<div>\n<iframe src={get_app_link(current_path, subdir)} width={width} height={height}></iframe>\n</div>"
+        return f"<div>\n<iframe src={get_app_link(current_path, subdir, "index.html")} width={width} height={height}></iframe>\n</div>"
+
+    @env.macro
+    def embed_code(subdir: str = None, language: str = 'python'):
+        current_path = Path(str(getattr(env, 'page'))) # " /blog/plotly-penguins-app.html"
+        if language == 'python':
+            with open(Path("site", get_app_link(current_path, subdir, "app.py"))) as f:
+                code_text =  f.read()
+            embed_text = "\n``` py title='app.py' linenums='1'\n" + code_text + "\n```\n"
+        else:
+            raise KeyError("Unsupported language")
+        return embed_text
 
     @env.macro
     def doc_env():
